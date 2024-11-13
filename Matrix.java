65338021cc534bb272e9146cad6f8a9586445a6d
corey <coreythehop@gmail.com>
2024-11-13 14:57:16 -0500

    Fixed Columns

--- Matrix.java
+++ Matrix.java
@@ -28,6 +28,19 @@ public class Matrix {
     }
   }
 
+  public void printDefaultValues() (
+    for (int row =0; row < dimension; row++) {
+      for (int column = 0; column < dimension; column++) {
+        if (row + column == dimension - 1) {
+          System.out.print("*" + 0 + "*\t");
+        } else {
+          System.out.print(0 + "\t");
+        }
+      }
+      System.out.println();
+    }
+  )
+
   public void flipMatrix() (
     int input = dimension * dimension;
     for (int row = 0; row < dimension; row++) {
