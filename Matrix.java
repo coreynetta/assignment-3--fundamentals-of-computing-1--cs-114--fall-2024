69e01a1d40098407ab4ad17bf13cc6b5176daf5f
corey <coreythehop@gmail.com>
2024-11-13 15:35:40 -0500

    Removed To Make Fixes to Syntax Error

--- Matrix.java
+++ Matrix.java
@@ -1,19 +1,6 @@
 import java.util.Scanner;
 
 public class Matrix {
-  private int [][] assignmentMatrix;
-  private int dimension;
-  public Matrix(int dimension) {
-    this.dimension = dimension;
-    assignmentMatrix = new int[dimension][dimension];
-    System.out.println("Dimensions of Matrix" + dimension + "*" + dimension + ":");
-  }
-  public void populateMatrix() {
-    int input = 1
-    for (int row = 0; row < dimension; row++) {
-      for (int column = 0; column < dimension; column++) {
-        assignmentMatrix[row][column] = input++;
-      }
     }
   }
 
