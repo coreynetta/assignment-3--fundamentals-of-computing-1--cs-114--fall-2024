63201cac7d998f6b548e55c4cce58d8e020702c6
corey <coreythehop@gmail.com>
2024-11-13 14:56:55 -0500

    Added Basic Matrix Pritning

--- Matrix.java
+++ Matrix.java
@@ -8,4 +8,24 @@ public class Matrix {
     assignmentMatrix = new int[dimension][dimension];
     System.out.println("Dimensions of Matrix" + dimension + "*" + dimension + ":");
   }
+  public void populateMatrix() {
+    int input = 1
+    for (int row = 0; row < dimension; row++) {
+      for (int column = 0; column < dimension; column++) {
+        assignmentMatrix[row][column] = input++;
+      }
+    }
+  }
+
+  public void printMatrix() {
+    for (int row = 0; row < dimension; row++) {
+      for (int column = 0; column < dimension; column++) {
+        if (row + column == dimension - 1) {
+          System.out.print(assignmentMatrix[row][column] + "\t");
+        }
+      }
+      System.out.println();
+    }
+  }
+
 }
