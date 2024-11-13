9ce49e51469ce0c160b9a8be60b163a903885eae
corey <coreythehop@gmail.com>
2024-11-13 14:56:08 -0500

    Added Scanner

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,21 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+
+    System.out.println("Please enter a positive number:");
+    int input = scanner.nextInt();
+
+    Matrix assignmentMatrix = new Matrix(input);
+
+    System.out.println("Default Matrix:");
+    assignmentMatrix.printDefaultValues();
+    assignmentMatrix.populateMatrix();
+    System.out.println("Normal Matrix:");
+    assignmentMatrix.printMatrix();
+    assignmentMatrix.flipMatrix();
+    System.out.println("Flipped Matrix:");
+    assignmentMatrix.printMatrix();
+
   }
 }
