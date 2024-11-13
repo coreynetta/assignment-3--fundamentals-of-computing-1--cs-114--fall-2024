10c3c9c919b83cab1c5b625d331a1acefce5ca1b
corey <coreythehop@gmail.com>
2024-11-13 16:14:13 -0500

    Deleted Flipped Matrix Original Code

--- Matrix.java
+++ Matrix.java
@@ -46,16 +46,6 @@ public class Matrix {
     }
 
     public void flipMatrix() {
-        int input = dimension * dimension;
-        for (int row = 0; row < dimension; row++) {
-            for (int column = 0; column < dimension; column++) {
-                if (row + column == dimension - 1) {
-                    continue;
-                }
-                assignmentMatrix[row][column] = input--;
-            }
-        }
-    }
 
     public void printFlippedMatrix() {
         for (int row = 0; row < dimension; row++) {
