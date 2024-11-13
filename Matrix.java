cc2f3052f7d09765d547159bb54ad79eba9a2280
corey <coreythehop@gmail.com>
2024-11-13 14:56:36 -0500

    Added Dimension Handler

--- Matrix.java
+++ Matrix.java
@@ -3,4 +3,9 @@ import java.util.Scanner;
 public class Matrix {
   private int [][] assignmentMatrix;
   private int dimension;
+  public Matrix(int dimension) {
+    this.dimension = dimension;
+    assignmentMatrix = new int[dimension][dimension];
+    System.out.println("Dimensions of Matrix" + dimension + "*" + dimension + ":");
+  }
 }
